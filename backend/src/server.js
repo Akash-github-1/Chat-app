c77426825b515db85b68b926a6f49e696cb282f9
Akash-github-1 <akumar95389@gmail.com>
2025-11-23 15:46:35 +0530

    server changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,16 +24,27 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 
-// make ready for deployment
-if(ENV.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// // make ready for deployment
+// if(ENV.NODE_ENV === "production"){
+//     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    // app.get("*",(req,res)=> {
-    //     res.sendFile(path.join(__dirname, "../frontend/disk/index.html"));
-    // })
-    app.get("*",(_,res)=> {
-        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
-    })
+//     // app.get("*",(req,res)=> {
+//     //     res.sendFile(path.join(__dirname, "../frontend/disk/index.html"));
+//     // })
+//     app.get("*",(_,res)=> {
+//         res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+//     })
+// }
+
+if (ENV.NODE_ENV === "production") {
+
+    const frontendPath = path.join(__dirname, "frontend", "dist");
+
+    app.use(express.static(frontendPath));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(frontendPath, "index.html"));
+    });
 }
 
 server.listen(PORT,() =>{
